394d2d1f4f8e18b61679843cdb20af249bd1d4f2
Peter Ciaccia <pnciaccia@gmail.com>
2021-09-20 21:05:09 -0400

    tracks some .py/.ipynb files

---
+++ main.py
@@ -0,0 +1,43 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+
+class Workspace(object):
+    """
+    A Workspace object captures all relevant information related  to the instance of organism you design.
+    :param object:
+    :return:
+    """
+
+    #TODO:
+
+
+class ImportWizard(object):
+
+    # contains
+
+    ### general ###
+    # TODO: Import .fasta as Seq object
+
+    ### Rna-seq ###
+    # TODO: Import .fastq
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
