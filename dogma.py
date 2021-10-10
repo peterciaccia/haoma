da1163c31aa57c4906a525efd401750603f9f71c
Peter Ciaccia <pnciaccia@gmail.com>
2021-10-10 14:22:03 -0400

    - creates dogma.py; populates with empty classes.

---
+++ dogma.py
@@ -0,0 +1,27 @@
+"""
+
+"""
+
+
+class StartTranscription:
+
+    def __init__(self):
+        pass
+
+
+class StopTranscription:
+
+    def __init__(self):
+        pass
+
+
+class StartTranslation:
+
+    def __init__(self):
+        pass
+
+
+class StopTranslation:
+
+    def __init__(self):
+        pass
