e6664c0720b02f2aefd135144ce27fe3006ce7a5
Peter Ciaccia <pnciaccia@gmail.com>
2021-10-11 14:44:07 -0400

    - adds logger

---
+++ log/conf.py
@@ -0,0 +1,27 @@
+default_config = {'disable_existing_loggers': False,
+                  'version': 1,
+                  'formatters':
+                      {
+                      'default':
+                          {
+                              'format': '%(asctime)s %(threadName)s [%(levelname)s] %(name)s: %(message)s'
+                          },
+                      },
+                  'handlers':
+                      {
+                          'console':
+                              {
+                                  'class': 'logging.StreamHandler',
+                                  'stream' : 'ext://sys.stderr',
+                                  'formatter': 'default'
+                              }
+                      },
+                  'loggers':
+                      {
+                          '':
+                              {
+                                  'handlers': ['console']
+                              }
+                      }
+                  }
+"""Dictionary to store logger configuration"""
