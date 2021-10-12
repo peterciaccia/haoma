f1ce63c54778645344c4b1b56721ed74321bcc6c
Peter Ciaccia <pnciaccia@gmail.com>
2021-10-12 17:41:04 -0400

    - makes declarative base for sqlalchemy - makes uniprot schema declaration - idmapping_selected.tab.example in external_data dir (and README)

---
+++ library/uniprot.py
@@ -0,0 +1,111 @@
+"""
+Created on 2021-10-12 by Peter Ciaccia
+"""
+
+from library import Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+uniprot_idmapping_path = "external_data/uniprot/knowledgebase/idmapping/README.txt"
+uniprot_idmapping = [
+    "UniProtKB-AC",
+    "UniProtKB-ID",
+    "GeneID", # (EntrezGene)
+    "RefSeq",
+    "GI",
+    "PDB",
+    "GO",
+    "UniRef100",
+    "UniRef90",
+    "UniRef50",
+    "UniParc",
+    "PIR",
+    "NCBI-taxon",
+    "MIM",
+    "UniGene",
+    "PubMed",
+    "EMBL",
+    "EMBL-CDS",
+    "Ensembl",
+    "Ensembl_TRS",
+    "Ensembl_PRO",
+    "Additional PubMed",
+]
+
+class UniprotIdMap(Base):
+    __tablename__ = 'uniprot_idmap'
+    # __table_args__ = {'mysql_engine':'InnoDB'}
+
+    UniProtKB_AC = Column(String(255), primary_key=True)
+    UniProtKB_ID = Column(String(255))
+    GeneID       = Column(Integer)
+    RefSeq       = Column(String(255))
+    GI           = relationship("Uniprot_GI", backref='uniprot_idmap')
+    PDB          = relationship("UniprotPdb", backref='uniprot_idmap')
+    GO           = relationship("UniprotGO", backref='uniprot_idmap')
+    UniRef100    = Column(String(255))
+    UniRef90     = Column(String(255))
+    UniRef50     = Column(String(255))
+    UniParc      = Column(String(255))
+    PIR          = relationship("UniprotPir", backref='uniprot_idmap')
+    NCBI_taxon   = Column(Integer())
+    MIM          = relationship("UniprotMim", backref='uniprot_idmap')
+    UniGene      = relationship("UniprotUniGene", backref='uniprot_idmap')
+    PubMed       = relationship("UniprotPubmed", backref='uniprot_idmap')
+    EMBL         = relationship("UniprotEmbl")
+    EMBL_CDS     = relationship("UniprotEmblCds", backref='uniprot_idmap')
+    Ensembl      = relationship("UniprotEnsembl")
+    Ensembl_TRS  = relationship("UniprotEnsemblTrs", backref='uniprot_idmap')
+    Ensembl_PRO  = relationship("UniprotEnsemblPro", backref='uniprot_idmap')
+    Additional_PubMed = relationship("UniprotAddtlPubmed", backref='uniprot_idmap')
+
+class UniprotIdmappingBase(Base):
+
+    id = Column(String(), primary_key=True)
+    parent_id = Column(String(255), ForeignKey('UniprotIdMap.UniProtKB_AC'))
+
+
+# TODO: Replace placeholder tables with foreign keys to other data sources
+
+class UniprotGI(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-gi'
+
+class UniprotGO(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-go'
+
+class UniprotPdb(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-pdb'
+
+class UniprotPir(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-pir'
+
+class UniprotMim(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-mim'
+
+class UniprotUniGene(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-unigene'
+
+class UniprotPubmed(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-pubmed'
+
+class UniprotEmbl(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-embl'
+
+class UniprotEmblCds(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-emblcds'
+
+class UniprotEnsembl(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-ensembl'
+
+class UniprotEnsemblTrs(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-ensembltrs'
+
+class UniprotEnsemblPro(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-ensemblpro'
+
+class UniprotAddtlPubmed(UniprotIdmappingBase):
+    __tablename__ = 'uniprot-additional-pubmed'
+
+
+Base.metadata.create_all(engine)
