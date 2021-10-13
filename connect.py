0aa350ba15aade56b21de0abd2b42a1bc9841431
Peter Ciaccia <pnciaccia@gmail.com>
2021-10-13 15:02:44 -0400

    - adds Dockerfile for Mysql server - adds docker-compose.yml. Not yet complete -

--- connect.py
+++ connect.py
@@ -7,7 +7,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
+from library import uniprot
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -25,17 +25,23 @@ db_host = config.get('host')
 db_port = config.get('port')
 db_name = config.get('database')
 
+# db_user = 'root'
+# db_pwd = 'ezekiel23_20!dsfdf'
+
 # specify connection string
 connection_str = f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
+# connection_str= f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/sqlalchemy'
 # connect to database
+print(connection_str)
 engine = create_engine(connection_str)
+uniprot.Base.metadata.create_all(bind=engine)
 # connection = engine.connect()
 # # pull metadata of a table
 # metadata = db.MetaData(bind=engine)
 # metadata.reflect(only=['test_table'])
 
-Session = sessionmaker(bind=engine)
-session = Session()
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
 
 
