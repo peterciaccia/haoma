d67c9bdfde62849f39e866b762d6e058ad9523ba
Peter Ciaccia <pnciaccia@gmail.com>
2021-12-23 09:29:35 -0500

    - TEST_make_tables.py: passes all tasks through run function (allows adding common decorators) - moves timer, log_ten_powers to test_tools.py - adds test template TEST_TEMPLATE.py

--- test_tools.py
+++ test_tools.py
@@ -4,38 +4,70 @@ Contains common functions used for several test scripts, like reading and writin
 """
 
 import os
+import time
+from functools import wraps
 from pathlib import Path
 import logging
 from datetime import datetime
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
-# Writes logs for most files except tests
 def get_log_path():
+    """
+    Writes logs for most files except tests
+    :return:
+    """
     log_dir = Path(os.getenv('PROJECT_LOG_DIR'))
     now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     log_name = Path(now)
     log_path = log_dir / log_name.with_suffix('.log')
     return log_path
 
-# Writes logs for test scripts
-def config_test_log(filename, logging_level=logging.DEBUG):
-    test_script_path = Path(filename)
-    test_dir = test_script_path.parent.absolute() / "logs"
-    test_filestem = test_script_path.stem
-    test_output_path = test_dir / Path(test_filestem).with_suffix(".log")
-    logging.basicConfig(filename=test_output_path,
-                        filemode="w",
-                        level=logging_level,
-                        format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-                        datefmt='%Y-%m-%d:%H:%M:%S'
-                        )
-    logging.info(str(datetime.now()))
-    return
+
+logging.basicConfig(filename=get_log_path(),
+                    filemode="w",
+                    level=logging.DEBUG,
+                    format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%Y-%m-%d:%H:%M:%S',
+                    # force=True
+                    )
+logger = logging.getLogger(__name__)
 
 
+def timer(task, *args, **kwargs):
+    """
+    decorator function that times other functions
+    :param task:
+    :param args:
+    :param kwargs:
+    :return:
+    """
 
-if __name__ == '__main__':
-    ImportWarning('Not calling from test script. Called by self')
-    config_test_log(__file__)
+    if logger.debug != logging.DEBUG:
+        return task(*args, **kwargs)
+
+    @wraps(task)
+    def wrap(*args, **kwargs):
+        start_time = time.time()
+        task_complete = task(*args, **kwargs)
+        end_time = time.time()
+        logger.debug(f'task {task.__name__} args: {args} kwargs: {kwargs} '
+                     f'took {end_time-start_time}.6f')
+        return task_complete
+    return wrap
+
+
+def log_ten_powers(powers):
+    """
+    Pass an iterable of ints by which 10 is exponentiated
+    :param powers: iterable of ints by which 10 is exponentiated
+    :return ten_powers: list of powers of ten
+    """
+
+    ten_powers = [10**ten_log for ten_log
+                  in powers
+                  if 10**ten_log % 1]
+
+    return ten_powers
--- tests/TEST_TEMPLATE.py
+++ tests/TEST_TEMPLATE.py
@@ -6,12 +6,13 @@ Findings: Chunk size of 10,000 was faster than chunk size of 100,000
 
 # internal modules
 import logging
-from test_tools import get_log_path, timer
+import test_tools
 # initial config needs to be defined for each test script
 from db import engine
 from db.models.models import RefSeq_to_Uniprot
+from test_tools import timer
 
-logging.basicConfig(filename=get_log_path(),
+logging.basicConfig(filename=test_tools.get_log_path(),
                     filemode="w",
                     level=logging.DEBUG,
                     format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
@@ -21,18 +22,11 @@ logging.basicConfig(filename=get_log_path(),
 logger = logging.getLogger(__name__)
 
 
-def make_table(chunk_size=10000, repopulate=True):
-
-    chunks = RefSeq_to_Uniprot.parse(debug=True, chunk_size=chunk_size)
-    RefSeq_to_Uniprot.populate(chunks, engine, debug=True, repopulate=repopulate)
-
-
 def rebuild_table(chunk_size=10000, repopulate=True):
     chunks = RefSeq_to_Uniprot.parse(debug=True, chunk_size=chunk_size)
     RefSeq_to_Uniprot.populate(chunks, engine, debug=True, repopulate=repopulate)
 
 
-##############################################################################
 ##############################################################################
 @timer
 def run(*args, **kwargs):
@@ -44,10 +38,8 @@ def run(*args, **kwargs):
     :return:
     """
     rebuild_table(*args, **kwargs, chunk_size=10000)
-    return
 
 
 
 if __name__ == '__main__':
     run()
-
