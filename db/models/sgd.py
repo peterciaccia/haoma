a6a5fd087589db1c1455dcbc8be1554169284976
Peter Ciaccia <pnciaccia@gmail.com>
2022-01-25 14:10:30 -0500

    - refactors db/models/sgd.py

--- db/models/sgd.py
+++ db/models/sgd.py
@@ -1,4 +1,7 @@
-
+"""
+Created by Peter Ciaccia
+This module contains Declarative mappings for SGD feature annotations.
+"""
 # built-ins
 import os
 from pathlib import Path
@@ -16,13 +19,13 @@ from db.utils import get_size
 import log.conf
 logger = log.conf.get_logger(module='test')
 
+
 class SgdFeature(Base):
 
     __tablename__ = 'sgd_features'
     __table_args__ = {'extend_existing': True}
     data_relative_path = Path('external_data/SGD/curation/chromosomal_feature/SGD_features.tab')
-    use_cols = list(range(14)) + [15]
-    # use_cols = [0, 1, 2, 3, 4, 6, 8, 9, 10, 11, 12, 13, 15]
+    _use_cols = list(range(14)) + [15]
     column_names = [
         'sgd_id',
         'feature_type',
@@ -62,14 +65,6 @@ class SgdFeature(Base):
     def __repr__(self):
         return f"<SgdFeatures(name={self.feature_name} sgd_id={self.sgd_id})>"
 
-    # @classmethod
-    # def make_alias_instance(cls, alias_list):
-    #     return [SgdAlias(alias) for alias in alias_list]
-    #
-    # @classmethod
-    # def make_secondary_sgd_id_instance(cls, ):
-    #     return [SecondarySgdId()]
-
     @classmethod
     def _get_simple_column_names(cls):
         _simple_column_tup = (0, 1, 2, 3, 4, 6, 8, 9, 10, 11, 12, 13, 14)
@@ -81,8 +76,7 @@ class SgdFeature(Base):
         return [cls.column_names[i] for i in _compound_column_tup]
 
     @classmethod
-    def instantiate_list(cls, name_list, DeclarativeTable, *args, **kwargs):\
-        # **{x: df_row[x] for x in cls.column_names}
+    def _instantiate_list(cls, name_list, DeclarativeTable, *args, **kwargs):\
         return [DeclarativeTable(**{}) for name in name_list]
 
     @classmethod
@@ -93,10 +87,9 @@ class SgdFeature(Base):
             cls.data_path,
             delimiter='\t',
             names=cls.column_names,
-            usecols=cls.use_cols,
+            usecols=cls._use_cols,
             nrows=nrows
         )
-
         # Splits "|"-delimited fields into lists
         _compound_column_names = cls._get_compound_column_names()
         for col in _compound_column_names:
@@ -125,52 +118,42 @@ class SgdFeature(Base):
                 SecondarySgdId.__table__,
                 SgdFeature.__table__
             ]
-            logger.debug(f"repopulating '{SgdFeature.__tablename__}'")
+            logger.debug(f"repopulating '{cls.__tablename__}'")
             Base.metadata.drop_all(bind=eng, tables=to_deletes)
-
         Base.metadata.create_all(bind=eng, checkfirst=True)
 
-        # get_size(RefSeq_to_Uniprot, verbose=True)
         df = SgdFeature.parse()
-        get_size(SgdFeature, debug=debug)
+        get_size(cls, debug=debug)
 
         with Session() as s:
             sgd_feature_list = []
 
             # imports simple features
             for i, df_row in df.iterrows():
-                # row_dict = {x: df_row[x] for x in cls.column_names}
-                # row = SgdFeature(**{x: df_row[x] for x in cls.column_names})
-                sgd_feature_list.append(SgdFeature(**{x: df_row[x] for x in cls._get_simple_column_names()}))
+                sgd_feature_list.append(cls(**{x: df_row[x]
+                                               for x in cls._get_simple_column_names()
+                                               }))
             s.add_all(sgd_feature_list)
             s.commit()
             many_to_one_sgd_objects_list = []
-            # test = SgdAlias('some_alias', 'some_sgd_feature_id')
 
             # imports compound features
             for i, df_row in df.iterrows():
-
-                foreign_key = "sgd_id"
+                foreign_key = 'sgd_id'
                 many_to_one_sgd_objects_list.extend(
                     [SgdAlias(alias=x, sgd_feature_id=df_row[foreign_key])
-                     for x in df_row["aliases"]])
+                     for x in df_row['aliases']])
                 many_to_one_sgd_objects_list.extend(
                     [SecondarySgdId(secondary_sgd_id=x, sgd_feature_id=df_row[foreign_key])
-                     for x in df_row["secondary_sgd_id"]]
+                     for x in df_row['secondary_sgd_id']]
                 )
-            pass
             s.add_all(many_to_one_sgd_objects_list)
             s.commit()
 
-        get_size(SgdFeature, debug=debug)
-
-        # with Session() as s:
-        #     s.add_all(row)
-        #     s.commit()
-
         # TODO: get # lines with a session call
-        _temp = '"Unimplemented"'
-        logger.debug(f'{_temp} lines added')
+        # _temp = '"Unimplemented"'
+        # get_size(cls, debug=debug)
+        # logger.debug(f"{_temp} lines added")
 
 
 """
@@ -180,21 +163,21 @@ Many-to-one relationship tables
 
 class SgdAlias(Base):
 
-    __tablename__ = "sgd_feature_aliases"
-    __table_args__ = {"extend_existing": True}
+    __tablename__ = 'sgd_feature_aliases'
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True)
     alias = Column(Text)
-    sgd_feature_id = Column(String(16), ForeignKey("sgd_features.sgd_id", ondelete="CASCADE"), nullable=False)
+    sgd_feature_id = Column(String(16), ForeignKey('sgd_features.sgd_id', ondelete='CASCADE'), nullable=False)
 
-    sgd_feature = relationship("SgdFeature", back_populates="aliases")
+    sgd_feature = relationship('SgdFeature', back_populates='aliases')
 
 
 class SecondarySgdId(Base):
 
-    __tablename__ = "secondary_sgd_id"
+    __tablename__ = 'secondary_sgd_id'
     __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True)
     secondary_sgd_id = Column(String(48))
-    sgd_feature_id = Column(String(16), ForeignKey("sgd_features.sgd_id", ondelete="CASCADE"), nullable=False)
+    sgd_feature_id = Column(String(16), ForeignKey('sgd_features.sgd_id', ondelete='CASCADE'), nullable=False)
 
     sgd_feature = relationship('SgdFeature', back_populates='secondary_sgd_id')
